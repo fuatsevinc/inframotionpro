f6494ddf97e53cac4c214c4dba12953193a1b59a
fuatsevinc <fuatsevinc@icloud.com>
2024-06-24 20:07:57 +0200

    update

--- bat/rd-mailform.php
+++ bat/rd-mailform.php
@@ -1,21 +1,29 @@
 <?php
+// Hata raporlamayı etkinleştir ve günlükle
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', '/path/to/your/php-error.log');
+
+// Form işleme kodu
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $phone = htmlspecialchars($_POST['phone']);
     $message = htmlspecialchars($_POST['message']);
-
-    $to = "fuatsevinc@icloud.com"; // Replace with your email
-    $subject = "Kontaktformular Nachricht von $name";
-    $body = "Name: $name\nEmail: $email\nTelefon: $phone\nNachricht:\n$message";
+    
+    $to = "fuatsevinc@icloud.com"; // Kendi e-posta adresinizle değiştirin
+    $subject = "$name isimli kişiden gelen iletişim formu mesajı";
+    $body = "İsim: $name\nEmail: $email\nTelefon: $phone\nMesaj:\n$message";
     $headers = "From: $email";
-
+    
     if (mail($to, $subject, $body, $headers)) {
-        echo "Nachricht erfolgreich gesendet.";
+        echo "Mesaj başarıyla gönderildi.";
     } else {
-        echo "Nachricht konnte nicht gesendet werden.";
+        echo "Mesaj gönderilemedi.";
     }
 } else {
-    echo "Ungültige Anforderung.";
+    echo "Geçersiz istek.";
 }
 ?>
