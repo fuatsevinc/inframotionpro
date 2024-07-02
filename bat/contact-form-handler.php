bb1cc2c211cd5d9f39eea5034b0f537c1b120aa1
fuatsevinc <fuatsevinc@icloud.com>
2024-07-03 01:21:20 +0200

    update

--- bat/contact-form-handler.php
+++ bat/contact-form-handler.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ini_set('log_errors', 1);
-ini_set('error_log', 'php-error.log'); // Hata log dosyası yolu
+ini_set('error_log', '/var/www/html/inframotionpro/php-error.log'); // Hata log dosyası yolu
 
 $response = array("status" => "", "message" => "");
 
@@ -27,6 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $response["status"] = "error";
         $response["message"] = "Mesaj gönderilemedi.";
+        error_log("Mail gönderilemedi.");
     }
 }
 
